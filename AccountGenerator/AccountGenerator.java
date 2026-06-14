2b2972078c82cbef3d9052aeaae69c0fa1f93b97
Jesse Madison <jessemadison@u.boisestate.edu>
2026-06-13 21:14:05 -0600

    lab warmup 3 done

---
+++ AccountGenerator/AccountGenerator.java
@@ -0,0 +1,35 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class AccountGenerator 
+{
+    public static void main(String[] args) 
+    {
+        try (Scanner kbd = new Scanner(System.in)) {
+            Random myGen = new Random();
+            
+            /* Input processing */
+            System.out.print("Enter your first name: ");
+            String userFirstName = kbd.nextLine();
+            System.out.print("Enter your last name: ");
+            String userLastName = kbd.nextLine();
+            
+            /* Convert user values to lower case */
+            userFirstName = userFirstName.toLowerCase();
+            userLastName = userLastName.toLowerCase();
+            
+            /* Grab specific characters from user input */
+            char firstLetterOfFirstName = userFirstName.charAt(0);
+            String firstFiveOfLastname = userLastName.substring(0, 5);
+            
+            /* Grab random int */
+            int myInt = myGen.nextInt(90) + 10;
+            
+            /* Output processing */
+            String username = firstLetterOfFirstName + firstFiveOfLastname + myInt;
+            System.out.println("Username: " + username);
+
+            kbd.close();
+        }
+    }
+}
